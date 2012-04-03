1de2e82e244e6b75c0c6a8bc479ee00d86bd41c9
Terry Schmidt <terry.m.schmidt@gmail.com>
2012-04-03 16:42:47 -0500

    Added some JS that will open external links in a new window

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
-/* Author:
+/* Author: Terry Schmidt
 
 */
 
-
-
-
-
-
-
+(function ($) {
+  
+  $('a[href^=http]').click(openInNewWindow);
+  
+  function openInNewWindow() {
+    var newWindow = window.open(this.getAttribute('href'), '_blank');
+    newWindow.focus();
+    return false;
+  }
+  
+})(jQuery);
